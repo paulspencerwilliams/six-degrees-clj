f8534c156e02806b5146ed0b80263cfe0e6943d5
Paul Williams <paul@paulswilliams.me.uk>
2014-03-09 17:04:01 +0000

    new project created

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to six-degrees-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/six_degrees_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns six-degrees-clj.core-test
+  (:require [clojure.test :refer :all]
+            [six-degrees-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
